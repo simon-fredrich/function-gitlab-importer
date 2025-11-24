ee90de3e76cecf1e259ed5258cdbbf15595d94ff
simon-fredrich <simon-fredrich@gmx.de>
2025-11-24 16:48:37 +0000

    delete obsolete comments

--- input/v1beta1/input.go
+++ input/v1beta1/input.go
@@ -8,12 +8,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// This isn't a custom resource, in the sense that we never install its CRD.
-// It is a KRM-like object, so we generate a CRD to describe its schema.
-
-// TODO: Add your input type here! It doesn't need to be called 'Input', you can
-// rename it to anything you like.
-
 // Input can be used to provide input to this Function.
 // +kubebuilder:object:root=true
 // +kubebuilder:storageversion
