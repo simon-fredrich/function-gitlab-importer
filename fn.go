b25cab21989c6f8eec6db544f5388929eb44f1ba
simon-fredrich <simon-fredrich@gmx.de>
2025-11-24 19:01:14 +0000

    test if import formatting is correct

--- fn.go
+++ fn.go
@@ -6,9 +6,10 @@ import (
 	"strconv"
 	"strings"
 
+	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
+
 	"github.com/crossplane/function-sdk-go/errors"
 	"github.com/crossplane/function-sdk-go/logging"
-	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/crossplane/function-sdk-go/request"
 	"github.com/crossplane/function-sdk-go/resource"
 	"github.com/crossplane/function-sdk-go/response"
