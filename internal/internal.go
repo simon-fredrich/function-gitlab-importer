7e505a27b740f971e908a0cac8a797bf7bb8cd3f
simon-fredrich <simon-fredrich@gmx.de>
2025-12-02 17:58:13 +0000

    make adding annotations easier

--- internal/internal.go
+++ internal/internal.go
@@ -58,20 +58,29 @@ func GetExternalNameFromObserved(obs resource.ObservedComposed) string {
 	return obs.Resource.GetAnnotations()["crossplane.io/external-name"]
 }
 
-// SetExternalNameOnDesired sets the external-name of a desired composed resource.
-func SetExternalNameOnDesired(des *resource.DesiredComposed, externalName string) error {
-	if externalName == "" {
-		return fmt.Errorf("external-name is empty")
-	}
-
+// AddAnnotationToDesired adds a custom annotation to a desired composed resource.
+// If the resource has no existing annotations, a new map is created.
+// The annotation is added using the provided key and value.
+//
+// This helper is useful in Crossplane functions for dynamically adding metadata
+// to desired resources during function execution.
+func AddAnnotationToDesired(des *resource.DesiredComposed, key string, value string) {
 	annotations := des.Resource.GetAnnotations()
 
 	if annotations == nil {
 		annotations = make(map[string]string)
 	}
 
-	annotations["crossplane.io/external-name"] = externalName
+	annotations[key] = value
 	des.Resource.SetAnnotations(annotations)
+}
 
+// SetExternalNameOnDesired sets the external-name of a desired composed resource.
+func SetExternalNameOnDesired(des *resource.DesiredComposed, externalName string) error {
+	if externalName == "" {
+		return fmt.Errorf("external-name is empty")
+	}
+
+	AddAnnotationToDesired(des, "crossplane.io/external-name", externalName)
 	return nil
 }
