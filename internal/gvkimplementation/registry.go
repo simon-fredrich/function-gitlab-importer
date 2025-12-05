67f137c2df18199af229cb69b6075cd8b377ca86
simon-fredrich <simon-fredrich@gmx.de>
2025-12-05 10:10:52 +0000

    create registry for implementations specific to gvk of resources

---
+++ internal/gvkimplementation/registry.go
@@ -0,0 +1,53 @@
+package gvkimplementation
+
+import (
+	providergroupsv1alpha1 "github.com/crossplane-contrib/provider-gitlab/apis/cluster/groups/v1alpha1"
+	providerprojectsv1alpha1 "github.com/crossplane-contrib/provider-gitlab/apis/cluster/projects/v1alpha1"
+	"github.com/simon-fredrich/function-gitlab-importer/internal/handler"
+	"github.com/simon-fredrich/function-gitlab-importer/internal/handler/gitlabhandler"
+	"github.com/simon-fredrich/function-gitlab-importer/internal/importer"
+	"github.com/simon-fredrich/function-gitlab-importer/internal/importer/gitlabimporter"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	gvkGroup   = providergroupsv1alpha1.GroupKubernetesGroupVersionKind
+	gvkProject = providerprojectsv1alpha1.ProjectGroupVersionKind
+)
+
+type implementation struct {
+	Handler  handler.Handler
+	Importer importer.Importer
+}
+
+var implementationByGVK = map[schema.GroupVersionKind]implementation{
+	gvkGroup: {
+		Handler:  &gitlabhandler.GroupHandler{},
+		Importer: &gitlabimporter.GroupImporter{},
+	},
+	gvkProject: {
+		Handler:  &gitlabhandler.ProjectHandler{},
+		Importer: &gitlabimporter.ProjectImporter{},
+	},
+}
+
+var allowedGVKs = map[schema.GroupVersionKind]struct{}{
+	gvkGroup:   {},
+	gvkProject: {},
+}
+
+// IsAllowed checks whether the given GroupVersionKind (GVK) is present
+// in the allowedGVKs set. It returns true if the GVK is permitted for
+// processing, and false otherwise.
+func IsAllowed(gvk schema.GroupVersionKind) bool {
+	_, ok := allowedGVKs[gvk]
+	return ok
+}
+
+// LookupByGVK retrieves the implementation (handler and importer) associated
+// with the given GroupVersionKind (GVK) from the implementationByGVK registry.
+// It returns the implementation and a boolean indicating whether the GVK was found.
+func LookupByGKV(gkv schema.GroupVersionKind) (implementation, bool) {
+	i, ok := implementationByGVK[gkv]
+	return i, ok
+}
